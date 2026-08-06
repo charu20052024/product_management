7566ad563f6b72b113c7b9ecb800f020b0c1a727
Charu <your-email@example.com>
2026-08-06 19:23:42 +0530

    push

--- includes/db.php
+++ includes/db.php
@@ -1,4 +1,7 @@
+
 <?php
+
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
 $conn = mysqli_connect(
     "mysql-production-18ee.up.railway.app",
@@ -7,10 +10,6 @@ $conn = mysqli_connect(
     "railway",
     33060
 );
-
-if (!$conn) {
-    die("DB ERROR: " . mysqli_connect_error());
-}
 
 echo "Database Connected";
 
