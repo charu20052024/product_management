ea03df06cba0a56bb60174ff38d89069756a40d6
Charu <your-email@example.com>
2026-08-06 16:21:07 +0530

    Fix Railway deployment

--- includes/db.php
+++ includes/db.php
@@ -1,21 +1,21 @@
 <?php
 
-echo "HOST: " . getenv("MYSQLHOST") . "<br>";
-echo "USER: " . getenv("MYSQLUSER") . "<br>";
-echo "DB: " . getenv("MYSQLDATABASE") . "<br>";
-echo "PORT: " . getenv("MYSQLPORT") . "<br>";
+$host = getenv("MYSQLHOST");
+$user = getenv("MYSQLUSER");
+$password = getenv("MYSQLPASSWORD");
+$db = getenv("MYSQLDATABASE");
+$port = getenv("MYSQLPORT");
 
 $conn = mysqli_connect(
-    getenv("MYSQLHOST"),
-    getenv("MYSQLUSER"),
-    getenv("MYSQLPASSWORD"),
-    getenv("MYSQLDATABASE"),
-    getenv("MYSQLPORT")
+    $host,
+    $user,
+    $password,
+    $db,
+    $port
 );
 
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
+if(!$conn){
+    die("Database Connection Failed: ".mysqli_connect_error());
 }
 
-echo "Database Connected Successfully";
 ?>
