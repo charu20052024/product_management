489660ebcbaf14febd0d4ae93430d8c90037b48a
Charu <your-email@example.com>
2026-08-06 19:32:50 +0530

    docker

--- includes/db.php
+++ includes/db.php
@@ -1,16 +1,23 @@
-
 <?php
 
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
-$conn = mysqli_connect(
-    "mysql-production-18ee.up.railway.app",
-    "root",
-    "EtlfKutXgOyqKGmngHwDRhVfRZVBFEuQ",
-    "railway",
-    33060
-);
+try {
 
-echo "Database Connected";
+    $conn = mysqli_connect(
+        "mysql-production-18ee.up.railway.app:33060",  // Railway public host
+        "root",                                  // MySQL username
+        "EtlfKutXgOyqKGmngHwDRhVfRZVBFEuQ",                     // Railway MySQL password
+        "railway",                               // Database name
+        33060                                    // Railway public port
+    );
+
+    echo "Database Connected";
+
+} catch (mysqli_sql_exception $e) {
+
+    die("Database Connection Failed: " . $e->getMessage());
+
+}
 
 ?>
