bb21337a64362aeb5e980b8cefcbb7641884e34c
Charu <your-email@example.com>
2026-08-06 19:47:42 +0530

    pushh

--- includes/db.php
+++ includes/db.php
@@ -1,22 +1,17 @@
-
 <?php
 
-$host = getenv("altaria.proxy.rlwy.net:54935");
-$user = getenv("root");
-$password = getenv("EtlfKutXgOyqKGmngHwDRhVfRZVBFEuQ");
-$database = getenv("railway");
-$port = getenv("54935");
+$host = "altaria.proxy.rlwy.net";
+$user = "root";
+$password = "EtlfKutXgOyqKGmngHwDRhVfRZVBFEuQ";
+$database = "railway";
+$port = 54935;
 
-$conn = mysqli_connect(
-    $host,
-    $user,
-    $password,
-    $database,
-    $port
-);
+$conn = mysqli_connect($host, $user, $password, $database, $port);
 
 if (!$conn) {
     die("Database connection failed: " . mysqli_connect_error());
 }
+
+echo "Database Connected";
 
 ?>
